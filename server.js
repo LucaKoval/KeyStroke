63ba410a1f2346efc7a17a5b1d646e8855845b71
Luca Koval <lucakoval@me.com>
2019-02-16 17:12:44 -0500

    Updating express

--- server.js
+++ server.js
@@ -1,40 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
-let body = ''
-
 app.get('/', function (req, res) {
-    console.log('they want to GET')
-    // if (req.method === 'POST') {
-    //     console.log('they want the POST')
-    //     req.on('data', function(data) {
-    //         console.log(data.toString())
-    //     })
-    // }
-    req.on('data', function(data) {
-        console.log('from get: ' + data.toString())
-        body += data.toString()
-    })
-
-    console.log('body (from get): ' + body)
-
-    res.render('index', {body: body})
-    // res.send('Hello World!')
+    console.log('they want the GET')
+    res.render('index')
 })
 
 app.post('/', function (req, res) {
+    let body = ''
+    
     console.log('they want the POST')
     req.on('data', function(data) {
-        // console.log(data.toString())
         body += data.toString()
     })
 
     console.log('body: ' + body)
 
-    // res.send('Hello World! From post!')
     res.render('index', {body: body})
 })
 
