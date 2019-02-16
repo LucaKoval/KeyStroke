45acfdbccffc625126b36a3ff422a1135f355ca7
Luca Koval <lucakoval@me.com>
2019-02-16 17:44:27 -0500

    Accessing data
    Example

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ let body = ''
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
+/* For actually displaying the pages, including when you click on the navbar links. */
 app.get('/index', function (req, res) {
     console.log('they want the GET: index')
-    res.render('pages/index')
+
+    var sports = [
+        { name: 'Football', difficulty: 3 },
+        { name: 'Soccer', difficulty: 5 },
+        { name: 'Curling', difficulty: 10 }
+    ];
+    var tagline = "When you herp, you derp.";
+
+    res.render('pages/index', {
+        sports: sports,
+        tagline: tagline
+    })
 })
 
 app.get('/data', function (req, res) {
