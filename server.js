a13dbb5ff5068b6986d79bad01f81c764955f181
Luca Koval <lucakoval@me.com>
2019-02-16 17:37:46 -0500

    Creating functional routing
    Navbar is modular and routing works

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+let body = ''
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    console.log('they want the GET')
-    res.render('index')
+app.get('/index', function (req, res) {
+    console.log('they want the GET: index')
+    res.render('pages/index')
+})
+
+app.get('/data', function (req, res) {
+    console.log('they want the GET: data')
+    res.render('pages/data')
+})
+
+app.get('/visuals', function (req, res) {
+    console.log('they want the GET: visuals')
+    res.render('pages/visuals')
 })
 
 app.post('/', function (req, res) {
-    let body = ''
-    
+
     console.log('they want the POST')
     req.on('data', function(data) {
         body += data.toString()
@@ -20,7 +31,7 @@ app.post('/', function (req, res) {
 
     console.log('body: ' + body)
 
-    res.render('index', {body: body})
+    res.render('pages/index', {body: body})
 })
 
 app.listen(8000, function () {
